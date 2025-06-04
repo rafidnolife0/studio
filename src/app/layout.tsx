5ae70cf522369687784423d8d6fe48246f93cf53
Esmita Rashid Pranty <pranty.geb@gmail.com>
2025-06-04 08:27:21 +0000

    kono change hoi ni

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,10 +18,10 @@ export default function RootLayout({
       <head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&display=swap" rel="stylesheet" />
-        <link href="https://fonts.googleapis.com/css2?family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
+        {/* Updated font links */}
+        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&family=Montserrat:wght@500;600;700&display=swap" rel="stylesheet" />
       </head>
-      <body className="font-body antialiased">
+      <body className="font-body antialiased"> {/* font-body will now pick up Roboto from tailwind.config.ts */}
         <AppProviders>
           {children}
           <Toaster />
